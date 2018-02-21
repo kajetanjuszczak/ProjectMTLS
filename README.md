231174bd427b1ffe178c2c0761ce9a08f183446e
xnestea <juszczak.kajetan@gmail.com>
2018-02-21 20:00:38 +0100

    21.02 final commit

--- README.md
+++ README.md
@@ -6,5 +6,6 @@ Diary:
     * Maybe try not to use the .readlines function
 * 21.01
     * Started thinking about what my output should look like, created dictionary relating aminoacid to its number. *think about map function as easier solution.
-    * Mostly reading stuff related to pandas, Scikit, SVM which is necessary to finish the praser.
+    * Mostly reading stuff related to pandas, Scikit, SVM which is necessary to finish the parser.
     * Try to use with open to make code shorter(def filetodictionary).
+    * My parser is now returning amino acid sequence and state in as list of numbers
--- project/general_scripts/parser.py
+++ project/general_scripts/parser.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import OneHotEncoder
 testDB = "D:/projekt/project/datasets/testDB.txt"
 
 def filetodictionary(filename):
@@ -16,11 +17,29 @@ def aatonumbers(dictionary):
     Create a list of numbers 1-20 with respect to aa names
     '''
     aadict = {'A' : 1, 'R' : 2, 'D' : 3, 'N' : 4, 'C': 5, 'E': 6, 'Q': 7, 'G': 8, 'H': 9, 'I': 10, 'L': 11, 'K': 12, 'M': 13, 'F': 14, 'P': 15, 'S': 16, 'T': 17, 'W': 18, 'Y': 19, 'V': 20}
-    print(aadict)
+    for key in dictionary:
+        seq = (dictionary[key])[0]
+        seqinnumbers = []
+        for aa in seq:
+            seqinnumbers.append(aadict[aa])
+        dictionary[key][0] = seqinnumbers
+    return dictionary
 def featurestonumbers(dictionary):
     '''
     Creates a list of numbers 0(burried) or 1(exposed) depending on the state of aa. (dict[list2])
     '''
+    statedict = {"B":0, "E":1}
+    for key in dictionary:
+        state1 = (dictionary[key])[1]
+        statenumbers = []
+        for state in state1:
+            statenumbers.append(statedict[state])
+        dictionary[key][1] = statenumbers
+    return dictionary
+
+def worksofar(filename):
+    return featurestonumbers(aatonumbers(filetodictionary(filename)))
+
 def selectwidowlen():
     windowlen = input("select window len:")
     return windowlen
@@ -28,12 +47,18 @@ def selectwidowlen():
 def createwindow(windowlen):
     '''
     creates the window desired by user
-    problem - what it it is not // 2?
-    problem2 - where to start window ?
     problem3 - where to store window ?
     and as a result produce an 2d array where a window of n residues is taken and the central one is described by feature in this place
     '''
-
+    window = 0
+    return window
+def SVMconventer(window, state):
+    '''
+    converts window into SVM accepted format(onehotencoder)
+    '''
+    
+    
 if __name__ == "__main__":
-    print(filetodictionary(testDB))
-    #aatonumbers(filetodictionary(testDB))
+    #print(filetodictionary(testDB))
+    #featurestonumbers(filetodictionary(testDB))
+    print(worksofar(testDB))
