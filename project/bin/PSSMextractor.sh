ed0c16513911bcd39ad7dc6b6535559696356329
xnestea <juszczak.kajetan@gmail.com>
2018-03-06 18:50:43 +0100

    6.03 commit

--- project/bin/PSSMextractor.sh
+++ project/bin/PSSMextractor.sh
@@ -3,6 +3,6 @@ export BLASTDB=/mnt/d/inne/uniref90
 cd ../datasets/SingleFastaForPSSM
 for entry in *.fasta
 do
-psiblast -query $entry -evalue 0.01 -db uniref90.fasta -num_iterations 3  -out ../PSSMDB/$entry.psiblast -out_ascii_pssm ../PSSMasci/$entry.pssm -num_threads=8
+psiblast -query $entry -evalue 0.01 -db uniref90.fasta -num_iterations 3  -out ../PSSMDB/$entry.psiblast -out_ascii_pssm ../PSSMasci/$entry.pssm -num_threads=4
 echo "done"
 done
