9f4a007ad0155bf14b8068435a4508801fd909c4
xnestea <juszczak.kajetan@gmail.com>
2018-03-07 13:03:53 +0100

    almost done PSSM parser

--- project/general_scripts/future_PSSM_Model.py
+++ project/general_scripts/future_PSSM_Model.py
@@ -20,7 +20,7 @@ for i, line in enumerate(open(testDB, "r")):
 ### find a file based on name of it
 listofseq = []
 for filename in listofnames:
-    matrix = np.genfromtxt("../datasets/PSSMasci/"+filename+".fasta.pssm", skip_header = 3, skip_footer = 6, dtype=None,usecols = range(22,42))
+    matrix = np.genfromtxt("../datasets/PSSMasci/"+filename+".fasta.pssm", skip_header = 3, skip_footer = 5, dtype=None,usecols = range(22,42))
     listofseq.append(matrix)
    
 ### create windows + match the states
@@ -36,17 +36,15 @@ for prot in listofseq:
             for nr in range(aa ,n):
                 window.append(zeros)
             window.extend(prot[0:aa + n + 1])
-            #window = np.append(listof0 + prot[0:aa + n + 1])
-            print(window)
-            
-            #window.extend(listof0 + prot[0:aa + n + 1])
-        #elif aa in range(len(prot) - n,len(prot)):
-        #    listof0 = zeros * (n + 1 + aa -len(prot))
-        #    window.extend(prot[aa - n:len(prot)] + listof0)
+        elif aa in range(len(prot) - n,len(prot)):
+            window.extend(prot[aa - n:len(prot)])
+            for nr in range(0,aa - len(prot) + n + 1):
+                window.append(zeros)
         else:
-            continue
+            window.extend(prot[aa - n :aa + n + 1])      
         listofwindows.append(window)
         print(np.array(listofwindows).shape)
     break
+    
 
-        
+      
